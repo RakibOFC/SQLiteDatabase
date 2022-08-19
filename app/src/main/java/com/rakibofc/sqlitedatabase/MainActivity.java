49aebef0dfc098985c068ef55d1d0c4a7c09f216
RakibOFC <rakibull2000@gmail.com>
2022-08-19 09:59:24 +0600

    Update whole project

--- app/src/main/java/com/rakibofc/sqlitedatabase/MainActivity.java
+++ app/src/main/java/com/rakibofc/sqlitedatabase/MainActivity.java
@@ -3,7 +3,6 @@ package com.rakibofc.sqlitedatabase;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -12,49 +11,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText ename, ecollege;
+    EditText ename, ecollege, eroll;
     Button binsert, bexit, bdisplay;
-    String nam, coll;
+    String nam, coll, roll;
     SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ename = findViewById(R.id.ename);
         ecollege = findViewById(R.id.ecollege);
+        eroll = findViewById(R.id.eroll);
         binsert = findViewById(R.id.binsert);
         bdisplay = findViewById(R.id.bdisplay);
         bexit = findViewById(R.id.bexit);
 
         db = openOrCreateDatabase("Mydb", MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS student(name VARCHAR, college VARCHAR); ");
+        db.execSQL("CREATE TABLE IF NOT EXISTS student(name VARCHAR, college VARCHAR, roll VARCHAR); ");
 
-        binsert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                nam = ename.getText().toString();
-                coll = ecollege.getText().toString();
-                db.execSQL("INSERT INTO student VALUES('" + nam + "','" + coll + "');");
-                Toast.makeText(getApplicationContext(), "Row Inserted", Toast.LENGTH_SHORT).show();
-            }
+        binsert.setOnClickListener(v -> {
+            nam = ename.getText().toString();
+            coll = ecollege.getText().toString();
+            roll = eroll.getText().toString();
+            db.execSQL("INSERT INTO student VALUES('" + nam + "','" + coll + "','" + roll + "');");
+            Toast.makeText(getApplicationContext(), "Row Inserted", Toast.LENGTH_SHORT).show();
         });
 
-        bdisplay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        bdisplay.setOnClickListener(v -> {
 
-                Intent intent = new Intent(getApplicationContext(), PreviewActivity.class);
-                startActivity(intent);
-                finish();
-            }
+            Intent intent = new Intent(getApplicationContext(), PreviewActivity.class);
+            startActivity(intent);
+            finish();
         });
 
-        bexit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.exit(0);
-            }
-        });
+        bexit.setOnClickListener(v -> System.exit(0));
     }
 }
--- app/src/main/java/com/rakibofc/sqlitedatabase/PreviewActivity.java
+++ app/src/main/java/com/rakibofc/sqlitedatabase/PreviewActivity.java
@@ -7,14 +7,13 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class PreviewActivity extends AppCompatActivity {
 
-    TextView tname, tcollege;
+    TextView tname, tcollege, troll;
     Button bprev, bnext, bback;
     SQLiteDatabase db;
 
@@ -25,63 +24,53 @@ public class PreviewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_preview);
         tname = findViewById(R.id.tname);
         tcollege = findViewById(R.id.tcollege);
+        troll = findViewById(R.id.troll);
         bprev = findViewById(R.id.bprev);
         bnext = findViewById(R.id.bnext);
         bback = findViewById(R.id.bback);
 
         db = openOrCreateDatabase("Mydb", MODE_PRIVATE, null);
 
+        @SuppressLint("Recycle")
         final Cursor c = db.rawQuery("select * from student", null);
+
         c.moveToFirst();
+
         tname.setText(c.getString(c.getColumnIndex("name")));
         tcollege.setText(c.getString(c.getColumnIndex("college")));
+        troll.setText(c.getString(c.getColumnIndex("roll")));
 
-        bback.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity
+        bback.setOnClickListener(v -> {
 
-                        .class);
-
-                startActivity(intent);
-                finish();
-            }
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
         });
 
-        bnext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    c.moveToNext();
-                    tname.setText(c.getString(c.getColumnIndex("name")));
-                    tcollege.setText(c.getString(c.getColumnIndex("college")));
+        bnext.setOnClickListener(v -> {
 
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "Last Record", Toast
+            try {
+                c.moveToNext();
+                tname.setText(c.getString(c.getColumnIndex("name")));
+                tcollege.setText(c.getString(c.getColumnIndex("college")));
+                troll.setText(c.getString(c.getColumnIndex("roll")));
 
-                            .LENGTH_LONG).show();
-                    e.printStackTrace();
-
-                }
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "Last Record", Toast.LENGTH_LONG).show();
+                e.printStackTrace();
             }
         });
-        bprev.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    c.moveToPrevious();
-                    tname.setText(c.getString(c.getColumnIndex("name")));
-                    tcollege.setText(c.getString(c.getColumnIndex("college")));
+        bprev.setOnClickListener(v -> {
 
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "First Record",
+            try {
+                c.moveToPrevious();
+                tname.setText(c.getString(c.getColumnIndex("name")));
+                tcollege.setText(c.getString(c.getColumnIndex("college")));
+                troll.setText(c.getString(c.getColumnIndex("roll")));
 
-                            Toast.LENGTH_LONG).show();
-
-                    e.printStackTrace();
-
-                }
-
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "First Record", Toast.LENGTH_LONG).show();
+                e.printStackTrace();
             }
         });
     }
